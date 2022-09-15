03a691703b174d06a4beb43df7e6b42e2f45653e
Vadym Kononenko <vadymk95@gmail.com>
2022-09-15 17:35:41 +0300

    master/chore/improved code

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,7 +1,8 @@
+import { FC } from 'react';
 import { useRoutes } from 'react-router-dom';
-import { routes } from './router';
+import { routes } from './routes';
 
-export const AppRouter = () => {
+export const AppRouter: FC = () => {
   const appRoutes = useRoutes(routes);
   return <>{appRoutes}</>;
 };
--- src/store/slices/postsSlice.ts
+++ src/store/slices/postsSlice.ts
@@ -16,19 +16,17 @@ export const postsSlice = createSlice({
     getPosts: (state) => {
       state.posts
     },
-    decrement: (state) => {
+    addPost: (state) => {
       state.posts;
     },
-    // Use the PayloadAction type to declare the contents of `action.payload`
-    incrementByAmount: (state, action: PayloadAction<number>) => {
+    removePost: (state, action: PayloadAction<number>) => {
       state.posts;
     },
   },
 });
 
-export const { getPosts, decrement, incrementByAmount } = postsSlice.actions;
+export const { getPosts, addPost, removePost } = postsSlice.actions;
 
-// Other code such as selectors can use the imported `RootState` type
 export const selectCount = (state: RootState) => state.posts;
 
 export default postsSlice.reducer;
