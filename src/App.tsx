cc3f16a083fd7a6a21f70d24a488d8989666f997
Vadym Kononenko <vadymk95@gmail.com>
2022-09-15 23:07:09 +0300

    master/style/App/set up layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react';
 import { Sidebar } from './components';
 import { AppRouter } from './router';
+import { styles } from './styles';
 
 export const App: FC = () => {
   return (
-    <div className="w-full overflow-hidden">
-      <Sidebar />
-      <AppRouter />
+    <div className="w-full flex overflow-hidden">
+      <div className={`${styles.paddingX}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </div>
     </div>
   );
 };
